6c92a1b5a307b19d9d88ec1a48e5563dec332656
tientulac <64886876+tientulac@users.noreply.github.com>
2021-06-14 20:44:07 +0700

    Upload

---
+++ Exam/Exam/DemoFileHandle.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Exam.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Exam
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DemoFileHandle : Page
+    {
+        public DemoFileHandle()
+        {
+            this.InitializeComponent();
+        }
+        private void write_file(object sender, RoutedEventArgs e)
+        {
+            FileHandleService.WriteFile("t2004e.txt", "Xin chao tat ca cac ban");
+        }
+        private async void read_File(object sender, RoutedEventArgs e)
+        {
+            string txt = await FileHandleService.ReadFile("employee.json");
+            txtBlock.Text = txt;
+        }
+    }
+}
---
+++ Exam/Exam/Services/FileHandleService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Exam.Services
+{
+    class FileHandleService
+    {
+        public static async void WriteFile(string fileName, string content)
+        {
+            var storage = Windows.Storage.ApplicationData.Current.LocalCacheFolder;
+            // tim den noi luu tru file trong may tinh
+            var demoFile = await storage.CreateFileAsync(fileName, Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(demoFile, content);
+        }
+        public static async Task<string> ReadFile(string fileName)
+        {
+            var storage = Windows.Storage.ApplicationData.Current.LocalCacheFolder;
+            var demoFile = await storage.CreateFileAsync(fileName, Windows.Storage.CreationCollisionOption.OpenIfExists);
+            return await FileIO.ReadTextAsync(demoFile);
+        }
+    }
+}
