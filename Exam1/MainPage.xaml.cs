66a5d73bc856781cb0cbcd83af1eef67c8013946
tientulac <64886876+tientulac@users.noreply.github.com>
2021-06-14 20:54:54 +0700

    Upload

---
+++ Exam1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            empList = new ListView();
+            this.InitJson();
+        }
+        private async void InitJson()
+        {
+            string FilePath = Path.Combine(Package.Current.InstalledLocation.Path, "employee.json");
+            using (StreamReader file = File.OpenText(FilePath))
+            {
+                var json = file.ReadToEnd();
+                Dictionary<string, object> result = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                string contacts = result["employee_list"].ToString();
+                List<data.Employee> objResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<data.Employee>>(contacts);
+                empList.ItemsSource = objResponse;
+                System.Diagnostics.Debug.WriteLine(Package.Current.InstalledLocation.Path);
+
+            }
+
+        }
+    }
+}
---
+++ Exam1/data/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.data
+{
+    [DataContract]
+    class Employee
+    {
+        [DataMember]
+        public string name
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string role
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public int birthyear
+        {
+            get;
+            set;
+        }
+    }
+}
